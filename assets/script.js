476123d4e5f6e38ad8caeafd3c759c42a5ba6591
jessica-calderon <calderonjessica13@yahoo.com>
2022-05-24 23:17:17 -0500

    add start, timer, and button variables

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,12 @@
 /* start quiz code */
+
+/* button variable */
+var start = document.querySelector("#start");
+
+/*  timer variables */
+var score = document.querySelector("#score");
+var time = document.querySelector(".timer");
+var secLeft = 80;
+
+/* start variable */
+var quizStart = document.querySelector("#challenge-section");
